f2bd3c79f474d43012087780e843527cbf972607
Franco Demarco <franco.demarco@muttdata.ai>
2023-09-21 12:32:11 -0300

    feat: print random facts to the console

--- src/franco_hypermodern_python/console.py
+++ src/franco_hypermodern_python/console.py
@@ -1,6 +1,13 @@
+"""The hypermodern Python project module."""
+
+
+import textwrap
+
 import click
+import requests
 
 from . import __version__
+from .constants import API_URL
 
 
 @click.command()
@@ -9,3 +16,11 @@ def main():
     """The hypermodern Python project.
     """
     click.echo("Hello World!")
+    with requests.get(API_URL, timeout=1) as response:
+        response.raise_for_status()
+        data = response.json()
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
---
+++ src/franco_hypermodern_python/constants.py
@@ -0,0 +1,3 @@
+"""This module includes all constants used throughout this project"""
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
