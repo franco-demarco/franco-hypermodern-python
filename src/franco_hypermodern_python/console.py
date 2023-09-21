47043a93a14df22c50b8abbe504fdf4096f3c081
Franco Demarco <franco.demarco@muttdata.ai>
2023-09-21 13:23:48 -0300

    style: apply formatting

--- src/franco_hypermodern_python/console.py
+++ src/franco_hypermodern_python/console.py
@@ -13,8 +13,7 @@ from .constants import API_URL
 @click.command()
 @click.version_option(version=__version__)
 def main():
-    """The hypermodern Python project.
-    """
+    """The hypermodern Python project."""
     click.echo("Hello World!")
     with requests.get(API_URL, timeout=1) as response:
         response.raise_for_status()
